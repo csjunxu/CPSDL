2edc6b8645b8d9baaab869a37755ab20fb5c1d18
XuJun <nankaimathxujun@gmail.com>
2016-09-30 16:27:08 +0800

    modify code

--- CPSDL_Dict_Train.m
+++ CPSDL_Dict_Train.m
@@ -10,37 +10,37 @@ task = 'SR';
 
 load Data/params.mat;
 if strcmp(task,'BID') == 1
-    load Data/EMGM_BID_8x8_5e5_100_20160922T135738.mat;
+    load Data/GMM_PG_8x8_100_20160930T014557.mat;
 elseif strcmp(task,'SR') == 1
     load Data/EMGM_SR_8x8_100_20160920T201654.mat;
 end
-%% Parameters Setting
-% tunable parameters
-par.rho = 0.05;
-par.lambda1         =       0.01;
-par.lambda2         =       0.01;
-par.mu              =       0.01;
-par.sqrtmu          =       sqrt(par.mu);
-par.nu              =       0.1;
-% fixed parameters
-par.epsilon         =        5e-3;
-par.cls_num            =    cls_num;
-par.step               =    2;
-par.win                =    8;
-par.nIter           =       100;
-par.t0              =       5;
-par.K               =       256;
-par.L               =       par.win * par.win;
-param.K = par.K;
-param.iter=300;
-param.lambda = par.lambda1;
-param.L = par.win * par.win;
-param.lambda2       =       par.lambda2;
-param.mode          = 	    2;       % penalized formulation
-param.approx=0;
-flag_initial_done = 0;
-paramsname = sprintf('Data/params.mat');
-save(paramsname,'par','param');
+% %% Parameters Setting
+% % tunable parameters
+% par.rho = 0.05;
+% par.lambda1         =       0.01;
+% par.lambda2         =       0.01;
+% par.mu              =       0.01;
+% par.sqrtmu          =       sqrt(par.mu);
+% par.nu              =       0.1;
+% % fixed parameters
+% par.epsilon         =        5e-3;
+% par.cls_num            =    cls_num;
+% par.step               =    2;
+% par.win                =    8;
+% par.nIter           =       100;
+% par.t0              =       5;
+% par.K               =       256;
+% par.L               =       par.win * par.win;
+% param.K = par.K;
+% param.iter=300;
+% param.lambda = par.lambda1;
+% param.L = par.win * par.win;
+% param.lambda2       =       par.lambda2;
+% param.mode          = 	    2;       % penalized formulation
+% param.approx=0;
+% flag_initial_done = 0;
+% paramsname = sprintf('Data/params.mat');
+% save(paramsname,'par','param');
 
 for i = 1 : par.cls_num
     XC = double(Xc{i});
